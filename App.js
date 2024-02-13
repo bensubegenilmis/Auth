e064251075cd34358c9a617523e16ea8d3ed7e34
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-02-13 12:02:09 +0300

    kullanicigirisscreen

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+
+const Stack = createNativeStackNavigator();
+
+function NormalStack() { 
+  return (
+    <Stack.Navigator
+    screenOptions={{
+      headerStyle:{
+        backgroundColor:'lightsteelblue',
+      },
+      headerTintColor: 'white',
+      contentStyle:{
+        backgroundColor:'white', //arka plan rengi
+      },
+    }}
+    >
+        <Stack.Screen name="Login" component={LoginScreen}
+        options={{
+          headerTitle:'Kullanıcı Giriş',
+        }}
+        />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <NormalStack />
+
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ components/Input.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View , TextInput} from 'react-native';
+import React from 'react';
+
+export default function Input({label, keyboardType, 
+    onUpdateValue, value, secure}) {
+  return (
+    <View style={styles.inputContainer}  >
+      <Text style={styles.label} >{label}</Text>
+      <TextInput 
+      style={styles.input}
+      autoCapitalize='none'
+      keyboardType={keyboardType}
+      onChangeText={onUpdateValue}
+      value={value}
+      secureTextEntry={secure}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    inputContainer:{
+        margin:8,
+    },
+    label:{
+        color:'white',
+        marginBottom: 3,
+    },
+    input:{
+        backgroundColor:'aliceblue',
+        paddingVertical: 8,
+        paddingHorizontal: 10,
+        borderRadius: 20,
+        fontSize: 16,
+    },
+});
